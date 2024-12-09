8eb160cceef460472249c959e8ec57075c333d28
Nilamadhab Senapati <nilamadhab.senapati@lenscorp.ai>
2024-12-09 10:54:53 +0530

    new changes

--- data/menuLinks.js
+++
@@ -1,8 +0,0 @@
-const Links = [
-    { name: "Home", to: "#hero-section", id: 1 },
-    { name: "About Us", to: "#about-us", id: 2 },
-    { name: "Our Works", to: "#our-works", id: 3 },
-    { name: "Contact Us", to: "#contact-us", id: 4 },
-  ];
-  
-  export default Links;
